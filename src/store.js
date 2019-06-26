bae503924078a55ab3fe664d71f78e93fbe8df63
Moataz <moataz.mohammady@gmail.com>
2019-06-26 16:29:40 +0200

    add `enhancer`

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers'
+import { createStore, applyMiddleware ,compose} from 'redux';
+import reducer from './reducers'
 const logger = store => next => action => {
     console.log('dispatching', action)
     let result = next(action)
     console.log('next state', store.getState())
     return result
 }
-const store = createStore(
-    reducers /* preloadedState */,
-    applyMiddleware( logger /* ...middleware */) /* any number of middlewares as arguments */
-);
 
-// const store = createStore(reducers /*Original state*/, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = window._s_REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,enhancer(
+    applyMiddleware(logger)
+));
+
 
 /*subscribe*/
 store.subscribe(() => {
