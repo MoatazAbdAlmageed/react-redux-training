8b444c231dbe7cb5f42463e40a73b6ee0006ee72
Moataz <moataz.mohammady@gmail.com>
2019-07-01 14:38:37 +0200

    - Add `bootstrap` - Add required components

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import './App.css';
 import Create from "./article/Create";
 import {Link, Route} from "react-router-dom";
+import SignIn from "./auth/SignIn";
+import SignUp from "./auth/SignUp";
+import Home from "./Home";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src="/favicon.ico" alt="React"/>
-                <h1>React</h1>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/create">Create Article</Link></li>
+        <div className="container">
+            <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+                <Link to="/">Home</Link>
+                <ul className="navbar-nav mr-aut">
+                    <li><Link to="signin">Sign in</Link></li>
                 </ul>
-
-            </header>
-
+            </nav>
 
             <Route path="/create" component={Create}/>
+            <Route path="/signin" component={SignIn}/>
+            <Route path="/signup" component={SignUp}/>
+            <Route path="/" exact component={Home}/>
+
 
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {Provider} from 'react-redux'
 import store from './store';
 import {BrowserRouter} from "react-router-dom";
 import App from "./components/App";
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 render(
