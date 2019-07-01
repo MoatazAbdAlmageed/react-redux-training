670229cc717d09a89c933e9701f957b045342c7c
Moataz <moataz.mohammady@gmail.com>
2019-07-01 11:46:20 +0200

    Add `redux-logger`

--- src/store.js
+++ src/store.js
@@ -1,12 +1,6 @@
 import { createStore, applyMiddleware ,compose} from 'redux';
 import reducer from './reducers'
-const logger = store => next => action => {
-    console.log('dispatching', action)
-    let result = next(action)
-    console.log('next state', store.getState())
-    return result
-}
-
+import logger from 'redux-logger'
 const enhancer = window._s_REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer,enhancer(
     applyMiddleware(logger)
