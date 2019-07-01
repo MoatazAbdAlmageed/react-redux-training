d3f831bbaf18985d19c8b895c26c8d9d8add6f71
Moataz <moataz.mohammady@gmail.com>
2019-07-01 12:03:12 +0200

    - Add `thunk` to `middleware` - Enhance app component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Moataz</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="/favicon.ico" alt="React"/>
+                <h1>React</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,23 +1,18 @@
 import {applyMiddleware, compose, createStore} from 'redux';
 import reducer from './reducers'
+import thunk from 'redux-thunk';
 
-const middlewares = [];
+const middleware = [thunk];
 
 if (process.env.NODE_ENV === `development`) {
     const {logger} = require(`redux-logger`);
-    middlewares.push(logger);
+    middleware.push(logger);
 }
 
-const enhancer = window._s_REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, enhancer(
-    applyMiddleware(...middlewares)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(...middleware)
 ));
 
-
-/*subscribe*/
-store.subscribe(() => {
-    // console.log("Store changed", store.getState());
-});
 
 export default store
