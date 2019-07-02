4d94b7abed17937565025841e944d47ca4496d6c
Moataz <moataz.mohammady@gmail.com>
2019-07-02 19:51:07 +0200

    Fix `eslint` errors

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function SignIn() {
-    return (
-        <div className="App">
-            <h1>Sign In</h1>
-            <Link to="/signup">Need an account?</Link>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Sign In</h1>
+      <Link to="/signup">Need an account?</Link>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function SignUp() {
-    return (
-        <div className="App">
-            <h1>Sign up</h1>
-            <Link to="/signIn">Have an account?</Link>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Sign up</h1>
+      <Link to="/signIn">Have an account?</Link>
+    </div>
+  );
 }
 
 export default SignUp;
