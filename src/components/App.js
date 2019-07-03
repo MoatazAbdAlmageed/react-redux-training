82808d50b7c2071c09554ec7ffb44d839d5eb2c3
Moataz <moataz.mohammady@gmail.com>
2019-07-03 12:58:05 +0200

    Revert "- Remove `.jsinspectrc.json`"
    This reverts commit df09b0dcf570bb6121e75c5c121286520ade9c54.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
 import Create from './article/Create';
 import SignIn from './auth/SignIn';
 import SignUp from './auth/SignUp';
@@ -16,26 +15,21 @@ function App() {
       <Container>
         <Row>
           <Col>
-            <Nav
-              activeKey="/"
-            >
-              <Nav.Item>
-                <Nav.Link href="/">Home</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="sign-in">Sign In</Nav.Link>
-              </Nav.Item>
-            </Nav>
 
-
+            <header className="App-header">
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/sign-in">Sign In</Link></li>
+              </ul>
+            </header>
           </Col>
         </Row>
 
         <Row>
           <Col>
             <Route path="/create-article" component={Create} />
-            <Route path="/sign-in" component={SignIn} />
-            <Route path="/sign-up" component={SignUp} />
+            <Route path="/Sign-in" component={SignIn} />
+            <Route path="/Sign-up" component={SignUp} />
             <Route path="/" exact component={Home} />
           </Col>
         </Row>
