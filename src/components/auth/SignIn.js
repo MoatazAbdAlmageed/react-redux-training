680815355efc8bf5242864da86e18fffb7164a58
Moataz <moataz.mohammady@gmail.com>
2019-07-04 13:46:35 +0200

    Convert `SignIn,SignUp` to be stateful components

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -4,30 +4,73 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function SignIn() {
-  return (
-    <div className="SignIn">
-      <h1>Sign In</h1>
-      <Link to="/sign-up">Need an account?</Link>
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Email Address"
-          aria-label="Email Address"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Password"
-          aria-label="Password"
-        />
-      </InputGroup>
 
-      <ButtonToolbar>
-        <Button size="lg" variant="info">Sign in</Button>
-      </ButtonToolbar>
-    </div>
-  );
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePassWordChange = this.handlePassWordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handlePassWordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    const { validated } = this.state;
+
+    return (
+      <div className="SignIn">
+        <h1>Sign In</h1>
+        <Link to="/sign-up">Need an account?</Link>
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={e => this.handleSubmit(e)}
+        >
+          <InputGroup className="mb-3">
+            <FormControl
+              required
+              type="email"
+              placeholder="Email Address"
+              aria-label="Email Address"
+              onChange={this.handleEmailChange}
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <FormControl
+              required
+              type="text"
+              placeholder="Password"
+              aria-label="Password"
+              onChange={this.handlePassWordChange}
+            />
+          </InputGroup>
+
+          <ButtonToolbar>
+            <Button type="submit" size="lg" variant="info">Sign in</Button>
+          </ButtonToolbar>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -4,36 +4,86 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Form from 'react-bootstrap/Form';
 
-function SignUp() {
-  return (
-    <div className="SignUp">
-      <h1>Sign Up</h1>
-      <Link to="/sign-in">Have an account?</Link>
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Username"
-          aria-label="Username"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Email Address"
-          aria-label="Email Address"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Password"
-          aria-label="Password"
-        />
-      </InputGroup>
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+    };
 
-      <ButtonToolbar>
-        <Button size="lg" variant="info">Sign up</Button>
-      </ButtonToolbar>
-    </div>
-  );
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePassWordChange = this.handlePassWordChange.bind(this);
+    this.handleUserNameChange = this.handleUserNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePassWordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleUserNameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    const { validated } = this.state;
+
+    return (
+      <div className="SignUp">
+        <h1>Sign Up</h1>
+        <Link to="/sign-in">Have an account?</Link>
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={e => this.handleSubmit(e)}
+        >
+          <InputGroup className="mb-3">
+            <FormControl
+              required
+              type="text"
+              placeholder="User Name"
+              aria-label="User Name"
+              onChange={this.handleUserNameChange}
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <FormControl
+              required
+              type="email"
+              placeholder="Email Address"
+              aria-label="Email Address"
+              onChange={this.handleEmailChange}
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <FormControl
+              required
+              type="text"
+              placeholder="Password"
+              aria-label="Password"
+              onChange={this.handlePassWordChange}
+            />
+          </InputGroup>
+
+          <ButtonToolbar>
+            <Button type="submit" size="lg" variant="info">Sign up</Button>
+          </ButtonToolbar>
+        </Form>
+      </div>
+    );
+  }
 }
-
 export default SignUp;
