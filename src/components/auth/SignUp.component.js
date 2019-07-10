25c8492b38dec72720a47932bff9e1ba81f7250c
Moataz <moataz.mohammady@gmail.com>
2019-07-10 13:35:23 +0200

    Init `SignUp`

--- src/components/auth/SignUp.component.js
+++ src/components/auth/SignUp.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
@@ -33,55 +33,63 @@ class SignUp extends React.Component {
   render() {
     const { validated } = false;
     const { username, email, password } = this.state;
+    const token = localStorage.getItem('token');
 
     return (
-      <div className="SignUp">
-        <h1>Sign Up</h1>
-        <Link to="/sign-in">Have an account?</Link>
-        <Form
-          name="SignUp"
-          novalidates={validated}
-          validated={validated}
-          onSubmit={e => this.handleSubmit(e)}
-        >
-          <InputGroup className="mb-3">
-            <FormControl
-              required
-              type="text"
-              name="username"
-              value={username}
-              placeholder="User Name"
-              aria-label="User Name"
-              onChange={this.handleChange}
-            />
-          </InputGroup>
-          <InputGroup className="mb-3">
-            <FormControl
-              required
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Email Address"
-              aria-label="Email Address"
-              onChange={this.handleChange}
-            />
-          </InputGroup>
-          <InputGroup className="mb-3">
-            <FormControl
-              required
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Password"
-              aria-label="Password"
-              onChange={this.handleChange}
-            />
-          </InputGroup>
+      <div>
 
-          <ButtonToolbar>
-            <Button type="submit" size="lg" variant="info">Sign up</Button>
-          </ButtonToolbar>
-        </Form>
+        { token && token === '123' ? <Redirect to="/home" />
+          : (
+            <div className="SignUp">
+              <h1>Sign Up</h1>
+              <Link to="/sign-in">Have an account?</Link>
+              <Form
+                name="SignUp"
+                novalidates={validated}
+                validated={validated}
+                onSubmit={e => this.handleSubmit(e)}
+              >
+                <InputGroup className="mb-3">
+                  <FormControl
+                    required
+                    type="text"
+                    name="username"
+                    value={username}
+                    placeholder="User Name"
+                    aria-label="User Name"
+                    onChange={this.handleChange}
+                  />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                  <FormControl
+                    required
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="Email Address"
+                    aria-label="Email Address"
+                    onChange={this.handleChange}
+                  />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                  <FormControl
+                    required
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="Password"
+                    aria-label="Password"
+                    onChange={this.handleChange}
+                  />
+                </InputGroup>
+
+                <ButtonToolbar>
+                  <Button type="submit" size="lg" variant="info">Sign up</Button>
+                </ButtonToolbar>
+              </Form>
+
+            </div>
+          )}
       </div>
     );
   }
